20882cac4202137a3f417f6f1068d1a40bbee443
Kaique Rodrigues Menezes <kaique.rodrigues.menezes@gmail.com>
2014-12-04 13:08:27 -0200

    Multiplicação de matriz

--- entrada.cc
+++ entrada.cc
@@ -1,134 +1,33 @@
-// Indiviso inteiro;
-// Booliano boolean;
-// Diade coracao;
-// Irresoluto Universo;
-// Grafema letra;
-// Manifesto poema;
-
-// Indiviso[10] array;
-
-// Indiviso MDC(Indiviso a, Indiviso b ) Preludio
-// 	Na hipotese de(b Equivalente a 0)
-// 		Restitua a;
-
-// 	Restitua MDC(b, a Remanescente b);
-// Epilogo
-
-// Primacial Preludio
-// 	// Testando o if
-// 	// coracao = 100.20;
-// 	// Na hipotese de ( coracao Equivalente a 100.10)
-// 	// 	Preludio
-// 	// 		Exclame "Foi";
-// 	// 	Epilogo
-// 	// Salvo
-// 	// 	Preludio
-// 	// 		Exclame "Não foi";
-// 	// 	Epilogo
-		
-// 	// inteiro = 100;
-// 	// // Testando o While
-// 	// Ao passo que(inteiro Superior a 0)
-// 	// Preludio
-// 	// 	Exclame inteiro "\n";
-// 	// 	inteiro = inteiro Menos 1;
-// 	// Epilogo
-
-// 	// // Testando o for
-// 	// Seja qual for( Indiviso i = 0; i = i Mais 1; Tal que i Inferior a 100  )
-// 	// 	Exclame i "\n";
-
-// 	// Tesytando o Do while
-// 	// Averigue >> inteiro;
-// 	// Realize
-// 	// Preludio
-// 	// 	Exclame inteiro "\n";
-// 	// 	inteiro = inteiro Menos 1;
-// 	// Epilogo
-// 	// Ao passo que(inteiro Superior a 0);
-// 	Indiviso [3][4] colecao;
-// 	inteiro=0;
-
-// 	Seja qual for( Indiviso i = 0 ; i = i Mais 1; Tal que i Inferior a 3) 
-// 	Preludio
-// 		Seja qual for( Indiviso j = 0 ; j = j Mais 1; Tal que j Inferior a 4)
-// 		Preludio
-// 			colecao[i][j] = inteiro;
-// 			inteiro = inteiro Mais 1;
-// 			Exclame colecao[i][j] "\n";
-// 		Epilogo
-// 	Epilogo
-// 	//Indiviso [3][4] colecao;
-// 	//inteiro=0;
-// 	//Seja qual for( Indiviso i = 0 ; i = i Mais 1; Tal que i Inferior a 3)
-// 	//	Seja qual for( Indiviso j = 0 ; j = j Mais 1; Tal que j Inferior a 4)
-// 	//	Preludio
-// 	//		colecao[i][j] = inteiro;
-// 	//		inteiro = inteiro Mais 1;
-// 	//	Epilogo
-// 	//Seja qual for( i = 0 ; i = i Mais 1; Tal que i Inferior a 3)
-// 	//	Seja qual for( j = 0 ; j = j Mais 1; Tal que j Inferior a 4)
-// 	//	Preludio
-// 	//		Exclame(colecao[i][j]);
-// 	//	Epilogo
-
-// 	//Teste do Switch
-// 	Indiviso x = 0;
-// 	Assemelhe (x)
-// 	Preludio
-// 		Sendo 0:
-// 			Exclame "Zero";
-// 		Sendo 1:
-// 			Exclame "Um";
-// 		Sendo 2:
-// 			Exclame "Dois!";
-// 		Sendo 3:
-// 			Exclame "Tres";
-// 		Padrao:
-// 			Exclame "???";
-// 	Epilogo
-// Epilogo
-
-Indiviso inteiro;
-Booliano boolean;
-Diade coracao;
-Irresoluto Universo;
-Grafema letra;
-Manifesto poema;
-
 Primacial Preludio
-	Indiviso magnitude= 3;
-	Indiviso dimensao = 4;
-	Indiviso grandeza = 10;
-	Indiviso acumulador;
-	Indiviso[magnitude][dimensao] matriz1;
-	Indiviso[dimensao][grandeza] matriz2;
-	Indiviso[magnitude][grandeza] produto;
-	inteiro=0;
-	Seja qual for( Indiviso i=0 ; i = i Mais 1; Tal que i Inferior a magnitude)            // Inicialização da primeira matriz
-		Seja qual for( Indiviso j=0 ; j = j Mais 1; Tal que j Inferior a dimensao)
-		Preludio
-			matriz1[i][j] = inteiro;
-			inteiro = inteiro Mais 1;
-		Epilogo
-	Seja qual for( i=0 ; i = i Mais 1; Tal que i Inferior a dimensao)			// Inicialização da segunda matriz
-		Seja qual for( j =0; j = j Mais 1; Tal que j Inferior a grandeza)
-		Preludio
-			matriz2[i][j] = inteiro;
-			inteiro = inteiro Mais 1;
-		Epilogo
-	Seja qual for( i =0; i = i Mais 1; Tal que i Inferior a magnitude)					//Multiplicação
-		Seja qual for( j=0 ; j = j Mais 1; Tal que j Inferior a grandeza)
-		Preludio
-			acumulador=0;
-			Seja qual for( Indiviso k=0 ; k = k Mais 1; Tal que k Inferior a dimensao)
-				acumulador = acumulador  Mais  matriz1[i][k] Vezes matriz2[k][j];
-			produto[i][j] = acumulador;
-			Exclame "Posicao";
-			Exclame i;
-			Exclame " - ";							// Escreve na Tela
-			Exclame j;
-			Exclame produto[i][j];
-			Exclame "\n" ;
-		Epilogo
+    Indiviso inteiro;
+    Indiviso magnitude= 3;
+    Indiviso dimensao = 4;
+    Indiviso grandeza = 10;
+    Indiviso acumulador;
+    Indiviso [magnitude][dimensao] matriz1;       // 3 por 4
+    Indiviso[dimensao][grandeza] matriz2;           // 4 por 10
+    Indiviso[magnitude][grandeza] produto;          // 3 por 10
+    inteiro=0;
+    Seja qual for( Indiviso i=0 ; i = i+1; tal que i Inferior a magnitude)            // Inicialização da primeira matriz
+        Seja qual for( Indiviso j=0 ; j = j+1; tal que j Inferior a dimensao)
+        Preludio
+            matriz1[i][j] = inteiro;
+            inteiro = inteiro+1;
+        Epilogo
+    Seja qual for( i=0 ; i = i+1; tal que i Inferior a dimensao)           // Inicialização da segunda matriz
+        Seja qual for( j =0; j = j+1; tal que j Inferior a grandeza)
+        Preludio
+            matriz2[i][j] = inteiro;
+            inteiro = inteiro+1;
+        Epilogo
+    Exclame "Chegou!!onze";
+    Seja qual for( i =0; i = i+1; tal que i Inferior a magnitude)                  //Multiplicação
+        Seja qual for( j=0 ; j = j+1; tal que j Inferior a grandeza)
+        Preludio
+            acumulador=0;
+            Seja qual for( Indiviso k=0 ; k = k+1; tal que k Inferior a dimensao)
+                acumulador = acumulador + matriz1[i][k]* matriz2[k][j];
+            produto[i,j] = acumulador;
+            Exclame "Posicao " i " - " j " : "produto[i][j] "\n";
+        Epilogo
 Epilogo
